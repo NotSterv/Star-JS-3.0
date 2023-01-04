3b1cdcc0a09b12db737559966fd1e6a28a784ad7
unknown <stervynjsajan@gmail.com>
2023-01-04 17:20:21 +0400

    bug fixes

--- commands/serverInfo.js
+++ commands/serverInfo.js
@@ -1,23 +1,37 @@
-const Discord = require('discord.js');
+const { stripIndent } = require('common-tags');
+const { MessageEmbed, Client, CommandInteraction, Guild } = require('discord.js');
 
 module.exports = {
 	name: 'serverinfo',
-	description: 'Show information about the current server',
-	execute(message, args) {
-		const server = message.guild;
-		if (!server) return message.channel.send("This command can only be used in a server.");
-		const embed = new Discord.EmbedBuilder()
-			.setTitle(server.name)
-			.setThumbnail(server.iconURL())
-			.addFields(
-				{ name: 'Server ID', value: server.id, inline: true },
-				{ name: 'Owner', value: server.owner.user.tag, inline: true },
-				{ name: 'Region', value: server.region, inline: true },
-				{ name: 'Members', value: server.memberCount, inline: true },
-				{ name: 'Roles', value: server.roles.cache.size, inline: true },
-				{ name: 'Channels', value: server.channels.cache.size, inline: true },
-			)
-			.setFooter(`Created on ${server.createdAt.toDateString()}`);
-		message.channel.send(embed);
-	},
-};
+	description: 'Displays information about the server',
+	async execute(message, guild) {
+	
+	const server = guild
+	const User = server.members.cache.filter(member => !member.user.bot).size;
+    const Bots = server.members.cache.filter(member => member.user.bot).size;
+    const Text = server.channels.cache.filter(channel => channel.type === 'GUILD_TEXT').size;
+    const Voice = server.channels.cache.filter(channel => channel.type === 'GUILD_VOICE').size;
+    const Category = server.channels.cache.filter(channel => channel.type === 'GUILD_CATEGORY').size;
+    const Stage = server.channels.cache.filter(channel => channel.type === 'GUILD_STAGE_VOICE').size;
+    const Channel = Text + Voice + Category + Stage
+    const Emoji = server.emojis.cache.size;
+    const Roles = server.roles.cache.size;
+
+    let embed = new MessageEmbed()
+      .setAuthor({ name: guild.name, iconURL: guild.iconURL() })
+      .setColor(guild.me.displayHexColor)
+      .setDescription(stripIndent`
+      **Name** : ${guild.name}
+      **Server ID** : ${guild.id}
+      **Owner** : ${await guild.fetchOwner().then(m => m.user.tag)}
+      **Total Members** : ${guild.memberCount} [${User} Users | ${Bots} Bots]
+      **Total Emojis** : ${Emoji}
+      **Total Roles** : ${Roles}
+      **Total Channels** : ${Channel} [${Text} Text | ${Voice} Voice | ${Category} Category | ${Stage} Stage]
+      **Server Creation Date** : <t:${time}>
+      `)
+      .setThumbnail(guild.iconURL())
+
+   message.channel.send(embed)
+  }
+}
